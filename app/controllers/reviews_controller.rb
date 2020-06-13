535d87940e6704194ca21eefebfdce073d50fb1d
Ridho <ridhohariono1904@gmail.com>
2020-06-14 02:41:44 +0700

    Reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action do
+    @purchase = Purchase.find_by_id(params[:purchase_id])
     case action_name.to_sym
     when :new, :create
       @review = Review.new
@@ -7,16 +8,27 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    @review.assign_attributes(review_params)
+    raise ActionController::RoutingError.new('Not Found') if @purchase.blank?
+    if @purchase.review_exist?
+      flash[:error] = @review.errors.full_messages.join(', ')
+      redirect_to product_purchase_path(@purchase.product_id, @purchase.id)
+    end 
   end
 
   def create
+    @purchase = Purchase.find(review_params[:purchase_id])
+    @review.assign_attributes(review_params)
+
     # TODO: Create the record in database
-    raise "TODO: Save to database: #{review_params}"
+    if @review.save
+    else
+      flash[:error] = @review.errors.full_messages.join(', ')
+    end
+    redirect_to product_purchase_path(@purchase.product_id, @purchase.id)
   end
 
   private
     def review_params
-      params.require(:review).permit(:purchase_id)
+      params.require(:review).permit(:purchase_id, :rating, :comment)
     end
 end
